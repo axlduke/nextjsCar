d628e297da9119a484cb61e96a0426f7bf8acd4a
ace malto <maltoace9@gmail.com>
2023-06-17 22:50:54 +0800

    updated

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,15 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters
+
     const headers = {
         'X-RapidAPI-Key': 'b360a8b9b5mshc29df5f12d72c17p170df0jsnb23a31e0d0e4',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers,
     });
 
